bc9e9a5561219e82831c97699c754698b61c81e0
RowanWhitethorn <joseantonio.diazv98@gmail.com>
2025-12-02 10:46:18 -0500

    Done (until tutorial)

--- app/tictactoe/page.jsx
+++ app/tictactoe/page.jsx
@@ -1,19 +1,61 @@
 "use client" 
-import { use, useState } from "react"; 
+import { useState } from "react"; 
+import "./styles.css";
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], // rows
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], // columns
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // diagonals
+    [2, 4, 6],
+  ];
+
+  for (let [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]; // "X" or "O"
+    }
+  }
+
+  return null;
+}
+
 function Square({value, onSquareClick}) { 
-    return <button onClick={onSquareClick}> {value ?? "-"} </button> } 
+    return <button className="square" onClick={onSquareClick}> {value ?? null} </button> } 
     
 function Board(){
      const [squares, setSquares] = useState(Array(9).fill(null)); 
      const [xIsNext, setXIsNext] = useState(true); 
+     const winner = calculateWinner(squares)
+     
      
 function handleClick (index){
-     const nextSquares = squares.slice(); nextSquares[index] = xIsNext ? "X" : "O"; 
-     setSquares(nextSquares); 
-     setXIsNext(!xIsNext);
-     } 
+    if (squares[index] || winner ){
+        return;}
+        const nextSquares = squares.slice(); 
+        nextSquares[index] = xIsNext ? "X" : "O"; 
+        setSquares(nextSquares); 
+        setXIsNext(!xIsNext);
+} 
+
+    let status;
+if (winner) {
+  status = `Winner: ${winner}`;
+} else {
+  status = `Next player: ${xIsNext ? "X" : "O"}`;
+}
+
+
+
+
      return(
          <>
+            <div>
+                {status}
+            </div>
             <div>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/> 
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/> 
@@ -32,4 +74,13 @@ function handleClick (index){
          </> 
         ); 
     } 
-    export default function TicTacToePage(){ return <Board/> }; 
+
+
+export default function TicTacToePage()
+ { return (
+     <>
+ <Board/> 
+
+</>
+)
+ }; 
